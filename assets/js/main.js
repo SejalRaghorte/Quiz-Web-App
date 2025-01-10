6f28b4eed3e7a7e2f395c8654471582947b1ef1b
Sejal Raghorte <raghortesejal308@gmail.com>
2025-01-10 14:15:09 +0530

    added logic for signup and login page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 // Core Logic: SignUp /Login
+
+document.getElementById("signup-form")?.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const role = document.getElementById("role").value;
+    signUp(username, password, role);
+  });
+  
+  document.getElementById("login-form")?.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("login-username").value;
+    const password = document.getElementById("login-password").value;
+    const role = document.getElementById("login-role").value;
+    login(username, password, role);
+  });
+  
